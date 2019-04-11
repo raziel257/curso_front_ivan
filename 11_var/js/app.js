7774d8d2c855a8923b8045cf0702884b4238da86
RAZIEL257 <raziel257a@gmail.com>
2019-04-11 12:05:26 +0200

    11_var

---
+++ 11_var/js/app.js
@@ -0,0 +1,9 @@
+/* en java script utilizamos variables para almacenar la informacion, a la que asignamos un valor. */
+let x = 76
+let y = 3
+let suma
+suma = x + y
+console.log(suma)
+/* console.dir(suma) es lo mismo que: console.log(suma) */
+/* tambien se puede escribir en line con ; let x=12; let y=3, let suma, suma =x+y; console.log(suma) */
+

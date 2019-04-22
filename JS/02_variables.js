64c3276ac61b0be67c15289d11d6aeca032b5781
RAZIEL257 <raziel257a@gmail.com>
2019-04-22 11:31:09 +0200

    variables

--- JS/02_variables.js
+++ JS/02_variables.js
@@ -1,3 +1,44 @@
 // ES6 se utilizan let y const para declarar variables
 
-let nombre
+let nombre //declarar
+nombre = 'Pepe' //inicializar.
+
+let edad = 35 // declaras e inicializas en la misma linea.
+
+// Tipos de datos
+//undefined
+let data
+console.log(data)
+console.log(typeof data) 
+
+// strings
+data = 'Hola ammigos'
+console.log(data)
+console.log(typeof data) 
+
+// numbers
+data = 35
+console.log(data)
+console.log(typeof data)
+
+// booleans
+data = true
+data = false
+console.log(data)
+console.log(typeof data)
+
+// objet
+data = { name: 'Pepe', age: 34}
+console.log(data)
+console.log(typeof data)
+
+// objet tipo array
+data = [23,45,78]
+console.log(data)
+console.log(typeof data)
+
+// Es un tipo de objeto
+function mostrar() {}
+
+console.log(mostrar)
+console.log(typeof mostrar)
---
+++ JS/03_funciones.js
@@ -0,0 +1,6 @@
+function suma(x,y) {
+    return x + y
+}
+console.log(suma(23, 20));
+
+

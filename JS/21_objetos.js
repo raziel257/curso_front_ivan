f7f38063a57a827f6574922331d9ca2997bc2649
RAZIEL257 <raziel257a@gmail.com>
2019-04-29 13:58:15 +0200

    objetos

---
+++ JS/21_objetos.js
@@ -0,0 +1,32 @@
+/**
+ * Notacion literal o JSON
+ */
+
+ //user1 = new Usuario
+ //user2 = new Usuario
+
+let user1 = {
+    nombre: 'Pepe',
+    edad: 24,
+    isAlumno: true,
+    direccion: {
+        Calle: 'Pez',
+        num: '3',
+        poblacion: 'Madrid'
+    },
+    saludar: function () {
+        console.log(`Hola soy ${this.nombre} y tengo ${this.edad} años, mí estatura es de ${this.altura} Cm.`)
+    },
+    altura : 179 
+}
+
+let user2 = {
+    nombre: 'Rosa',
+    edad: 27,
+    curso: 'Front'
+}
+
+user1.nombre = 'José'
+user1.curso = 'Back'
+
+user1.saludar()

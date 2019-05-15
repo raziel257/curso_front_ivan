23df9634b711419fe2bd11b4606c54a679a1d96a
RAZIEL257 <raziel257a@gmail.com>
2019-05-15 13:28:12 +0200

    biding

--- JS_web/08_mostrar.js/js/controller.js
+++ JS_web/08_mostrar.js/js/controller.js
@@ -2,28 +2,32 @@ export function controller () {
 
     console.log('Controller cargado')
 
-    let nombre = ''
-    let msginicial = ''
+    /** Declarar variables */
+    let isVisibleDiv = false
+    let msg = ['[...Mostrar menos]', '[Mostrar más...]']
+    
     /**Nodos del DOM */
-    const inNombre = document.querySelector('#in-nombre')
-    const btnBorrar = document.querySelector('#btn-borrar')
-    const output = document.querySelector('#output')
-
-    msginicial = output.innerHTML + ' '
+    let nodos = {
+        btnCambiar: document.querySelector('#btn-cambiar'),
+        divHide: document.querySelector('#div-hide')
+    }
+    
+    nodos.btnCambiar.innerHTML = msg[1]
 
     /** Asignar manejadores de eventos */
+    nodos.btnCambiar.addEventListener('click', cambiar)
 
-    inNombre.addEventListener('input', oninputNombre)
-    btnBorrar.addEventListener('click', borrar)
-    
-    function oninputNombre () {
-        nombre = inNombre.value
-        output.innerHTML = msginicial + nombre.toUpperCase()
+    function cambiar() {
+        /* let i = 0
+        if (isVisibleDiv) {
+            i = 1
+        }  */
         
+        nodos.btnCambiar.innerHTML = msg[isVisibleDiv ? 1 : 0]
+        nodos.divHide.classList.toggle('hide')
+        isVisibleDiv = !isVisibleDiv
     }
-    function borrar () {
-        inNombre.value = ''
-        oninputNombre()
-    }
+
+    
     
 }

ba17ceb273499609395f3e61d00aa8b398269883
RAZIEL257 <raziel257a@gmail.com>
2019-06-03 13:59:27 +0200

    Ejercicio 030619 casa

---
+++ JS_web/14_enlaces/js/controller.js
@@ -0,0 +1,14 @@
+export function controller ( {
+    
+    // Procedimiento habitual
+
+    // 1. Referencia a los Nodos del DOM que me interesan
+    const aNodos... = document.querySelectorAll('....')
+   /*  const aNodosRespuesta = document.querySelectorAll('.respuesta') */
+
+    
+    // 2. Definir los manejadores de eventos
+
+    aNodos....forEach( (item) => { item.addEventListener('click', onClick) })
+ // 3. Implemantar los manejadores de eventos
+})
